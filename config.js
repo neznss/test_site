9a2746228024a3f5e855df31e3abb2e2f59e201f
nezns <165576495+neznss@users.noreply.github.com>
2025-06-25 13:52:36 +0500

    key youtube config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // Unsplash: https://unsplash.com/developers
 // YouTube Data API: https://console.cloud.google.com/apis
 const UNSPLASH_ACCESS_KEY = 'ВАШ_UNSPLASH_KEY';
-const YOUTUBE_API_KEY   = 'ВАШ_YOUTUBE_KEY';
+const YOUTUBE_API_KEY   = 'AIzaSyBk9zvvoZKDcveIGLnXqqg-YC_yDSA6nTc';
